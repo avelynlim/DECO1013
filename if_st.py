9568cd3dea36cd65f07bd4a94f414af68f03acd9
avelynlim <70922138+avelynlim@users.noreply.github.com>
2020-10-14 12:48:51 +0900

    Add files via upload

---
+++ if_st.py
@@ -0,0 +1,38 @@
+from microbit import *
+import music
+import neopixel
+import random
+from random import randint
+
+# Setup the Neopixel strip on pin0 with a length of 8 pixels
+np = neopixel.NeoPixel(pin1, 30)
+notes = [
+    'c4:1', 'e', 'g', 'c5', 'e5', 'g4', 'c5', 'e5', 'c4', 'e', 'g', 'c5', 'e5', 'g4', 'c5', 'e5',
+    'c4', 'd', 'a', 'd5', 'f5', 'a4', 'd5', 'f5', 'c4', 'd', 'a', 'd5', 'f5', 'a4', 'd5', 'f5',
+    'b3', 'd4', 'g', 'd5', 'f5', 'g4', 'd5', 'f5', 'b3', 'd4', 'g', 'd5', 'f5', 'g4', 'd5', 'f5',
+    'c4', 'e', 'g', 'c5', 'e5', 'g4', 'c5', 'e5', 'c4', 'e', 'g', 'c5', 'e5', 'g4', 'c5', 'e5',
+    'c4', 'e', 'a', 'e5', 'a5', 'a4', 'e5', 'a5', 'c4', 'e', 'a', 'e5', 'a5', 'a4', 'e5', 'a5',
+    'c4', 'd', 'f#', 'a', 'd5', 'f#4', 'a', 'd5', 'c4', 'd', 'f#', 'a', 'd5', 'f#4', 'a', 'd5',
+    'b3', 'd4', 'g', 'd5', 'g5', 'g4', 'd5', 'g5', 'b3', 'd4', 'g', 'd5', 'g5', 'g4', 'd5', 'g5',
+    'b3', 'c4', 'e', 'g', 'c5', 'e4', 'g', 'c5', 'b3', 'c4', 'e', 'g', 'c5', 'e4', 'g', 'c5',
+    'a3', 'c4', 'e', 'g', 'c5', 'e4', 'g', 'c5', 'a3', 'c4', 'e', 'g', 'c5', 'e4', 'g', 'c5',
+    'd3', 'a', 'd4', 'f#', 'c5', 'd4', 'f#', 'c5', 'd3', 'a', 'd4', 'f#', 'c5', 'd4', 'f#', 'c5',
+    'g3', 'b', 'd4', 'g', 'b', 'd', 'g', 'b', 'g3', 'b3', 'd4', 'g', 'b', 'd', 'g', 'b'
+]
+while True:
+
+    if button_a.is_pressed():
+        music.play(notes)
+# play Prelude in C.
+    elif button_b.is_pressed():
+        for pixel_id in range(0, len(np)):
+            red = randint(0, 60)
+            green = randint(0, 60)
+            blue = randint(0, 60)
+        # Assign the current LED a random red, green and blue value between 0 and 60
+        np[pixel_id] = (red, green, blue)
+
+        # Display the current pixel data on the Neopixel strip
+        np.show()
+
+    sleep(1000)
